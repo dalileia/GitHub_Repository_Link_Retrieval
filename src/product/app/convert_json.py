eb045ce85252677fb68d417ddc716eda7c6f49bf
dalileia <dalileia>
2024-02-06 08:31:56 -0300

    fix errors in dates

--- src/product/app/convert_json.py
+++ src/product/app/convert_json.py
@@ -4,8 +4,8 @@ class ConvertJson():
 
     def convert_json():
         
-        input_file = '/product/app/data/input_github_search.json'
-        output_file = '/product/app/data/github_search.json'
+        input_file = '/product/app/data/input_link_list.json'
+        output_file = '/product/app/data/link_list.json'
 
         json_list = []
 
@@ -22,4 +22,4 @@ class ConvertJson():
             file.write(''.join(json_list))
             file.write(']')
 
-        os.remove('/product/app/data/input_github_search.json')
+        os.remove('/product/app/data/input_link_list.json')
--- src/product/app/run.py
+++ src/product/app/run.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     
     start_date = ''
     finish_date = ''
-    phrase = 'search test'
-    token = ''
+    phrase = 'search test api github'
+    token = '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'
 
-    ScrapeGithub.check_search(phrase,token,start_date,finish_date)
+    ScrapeGithub.check_and_search(phrase,token,start_date,finish_date)
     

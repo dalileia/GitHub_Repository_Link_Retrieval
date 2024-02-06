7da5c5dc232397df4f53e682dfb6034e980d089c
dalileia <dalileia>
2024-02-06 08:39:23 -0300

    fix errors

--- src/product/app/run.py
+++ src/product/app/run.py
@@ -4,10 +4,11 @@ from github_api import ScrapeGithub
 
 if __name__ == '__main__':
     
+    #Date: YYYY-MM-DD
     start_date = ''
     finish_date = ''
     phrase = 'search test api github'
-    token = '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'
+    token = ''
 
     ScrapeGithub.check_and_search(phrase,token,start_date,finish_date)
     
